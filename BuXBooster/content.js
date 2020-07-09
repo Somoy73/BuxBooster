7b8a9836e8f5f4a7202e4c2402f4be3afd0bc953
Somoy Subandhu <Somoy97@gmail.com>
2020-07-09 10:56:30 +0600

    small fixes

--- BuXBooster/content.js
+++ BuXBooster/content.js
@@ -56,12 +56,12 @@ chrome.runtime.onMessage.addListener(function (req){
         else{
             var context = new (window.AudioContext || window.webkitAudioContext),
             result = {
-            context: context,
-            source: context.createMediaElementSource(mediaElem),
-            gain: context.createGain(),
-            media: mediaElem,
-            amplify: function(multiplier) { result.gain.gain.value = multiplier; },
-            getAmpLevel: function() { return result.gain.gain.value; }
+                context: context,
+                source: context.createMediaElementSource(mediaElem),
+                gain: context.createGain(),
+                media: mediaElem,
+                amplify: function(multiplier) { result.gain.gain.value = multiplier; },
+                getAmpLevel: function() { return result.gain.gain.value; }
             };
             result.source.connect(result.gain);
             result.gain.connect(context.destination);
